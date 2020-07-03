0f4785f46ddab36989eba8b3438005d3a2f5ac6e
MarilenaRoque <marilenas@id.uff.br>
2020-07-03 18:01:18 -0300

    fixing zipcode format

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,6 +3,7 @@ puts 'Event Manager Initialized'
 
 content = CSV.open "../data.csv", headers: true, header_converters: :symbol
 content.each{ |count|
-  name = count[:last_name]
-  puts name
+  zip = count[:zipcode].to_s
+  zip = "0" + zip until zip.length == 5
+  puts "Guest: #{count[:first_name]} Zip Code: #{zip}"
 }
